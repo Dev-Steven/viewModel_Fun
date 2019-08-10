227012eaf207348ab525f1dc9b64503577320b66
Steven <steven.chung96@yahoo.com>
2019-08-10 15:39:37 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viewModelFun.Models;
+
+namespace viewModelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet ("")]
+        public IActionResult Index()
+        {
+            string message = "Hello hello helooooo!?";
+            return View("Index", message);
+        }
+
+        [HttpGet ("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] numbers = new int[]
+            {
+                1, 6, 8, 24
+            };
+            return View("Numbers", numbers);
+        }
+
+        [HttpGet ("users")]
+        public IActionResult Users()
+        {
+            User steven = new User("Steven", "Chung");
+            User bob = new User("Bob", "G.");
+            User tom = new User("Tom", "C.");
+
+            List<User> users = new List<User>()
+            {
+                steven, bob, tom
+            };
+
+            return View("Users", users);
+        }
+
+        [HttpGet ("user")]
+        public IActionResult User()
+        {
+            User steven = new User("Steven", "Chung");
+            return View("User", steven);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+namespace viewModelFun.Models
+{
+    public class User
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+
+        public User(string fn, string ln)
+        {
+            FirstName = fn;
+            LastName = ln;
+        }
+    }
+
+
+}
